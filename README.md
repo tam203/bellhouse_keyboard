18616d24031864c22986d777132ee434dc95502b
Theo McCaie <tam203@gmail.com>
2017-10-19 09:38:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bellhouse_keyboard
---
+++ index.js
@@ -0,0 +1,34 @@
+const NUM_BELLS = 26;
+const STRIKE_URL = "http://xxxxx.ngrok.io/strike/";
+const CONTAINER = $('#keyboard');
+let bells = {};
+
+function strike(letter) {
+    let bell = bells[letter];
+    if (bell) {
+        console.log(bell);
+        jQuery.get(STRIKE_URL + bell.i);
+
+        bell.element.fadeIn(100).fadeOut(100);
+    }
+};
+
+
+for (var i = 0; i < NUM_BELLS; i++) {
+    let key = String.fromCharCode(97 + i)
+    let color = Math.floor(0xFFFFFF * Math.random()).toString(16);
+    let element = $(`<section class="key" style='background-color:#${color};'>${key}</section>`);
+    bells[key] = {
+        i: i,
+        letter: key,
+        element: element
+    };
+    element.click(() => { strike(key) });
+    CONTAINER.append(element);
+}
+
+
+
+Rx.Observable.fromEvent(document, "keypress").subscribe((e) => {
+    strike(e.key);
+});
